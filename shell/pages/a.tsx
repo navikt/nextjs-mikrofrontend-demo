ca5c443d61ae8c7895ca1af367c80da4e60cdd29
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-10-06 15:16:43 +0200

    Utvid shell appen

---
+++ shell/pages/a.tsx
@@ -0,0 +1,7 @@
+import MicrofrontendA from "http://localhost:7100/app-a/bundle.js";
+
+const A = () => {
+  return <MicrofrontendA />;
+};
+
+export default A;
---
+++ shell/pages/b.tsx
@@ -0,0 +1,7 @@
+import MicrofrontendB from "http://localhost:7100/app-a/bundle.js";
+
+const B = () => {
+  return <MicrofrontendB />;
+};
+
+export default B;
