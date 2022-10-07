c067c1413f6f37882215dc317c414be5470be46e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-10-07 09:16:43 +0200

    Clean up

--- shell/pages/index.tsx
+++ shell/pages/index.tsx
@@ -3,11 +3,9 @@ import MicrofrontendB from "http://localhost:7200/app-b/bundle.js";
 
 export default function Home() {
   return (
-    <div className="container">
-      <main>
-        <MicrofrontendA />
-        <MicrofrontendB />
-      </main>
-    </div>
-  )
+    <main>
+      <MicrofrontendA />
+      <MicrofrontendB />
+    </main>
+  );
 }
